bb56ae57f8a24e8873a2f6195431d804b6980b50
AbhishekKr-Jha <akjha4127@gmail.com>
2024-02-12 20:08:53 +0530

    deploy_try.v7.3

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -27,13 +27,13 @@ zIndex:"200"
     }
   return ( 
    <>
-     
+     <div class="logoBox" >
    <img style={styleImg}  src='https://lh3.googleusercontent.com/oDoIif4GV1dMAVqNocJFpT5wc4QsOpuIJjqYSsnX1txTA_Klq1KAkUo-QzvRYwqmt64pDaBy53MCj8znPZfcKDuKZe9nL7lmcuXkPA' alt='err'/>
 
    <div className="MenuIcon" style={styleIcon}>
     <Box><IconButton onClick={showNav}><MenuIcon sx={{ fontSize:"28px"}}/></IconButton></Box>
    </div>
-
+   </div>
    </>
   )
 }
