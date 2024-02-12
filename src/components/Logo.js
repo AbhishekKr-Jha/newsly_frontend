876890402cdeaf6e5193ea2ae738556c214effe0
AbhishekKr-Jha <akjha4127@gmail.com>
2024-02-12 19:59:59 +0530

    deploy_try.v7.2

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,6 +7,7 @@ export default function Logo() {
   const showNav=()=>{
               const navItem=document.getElementById('vnav')
     navItem.style.display = "block"; 
+    
     console.log("clicked")
   }
     const styleImg={
@@ -14,6 +15,7 @@ width:"45px",
 position:"absolute",
 top:"6px",
 left:"5px",
+zIndex:"200"
     }
     const styleIcon={
       width:"45px",
@@ -21,7 +23,7 @@ position:"absolute",
 top:"6px", 
 right:"5px" ,
 cursor:"pointer",
-
+zIndex:"200"
     }
   return ( 
    <>
